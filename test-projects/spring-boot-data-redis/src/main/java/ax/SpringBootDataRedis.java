44bf49648151983897b6a7f97eec0bb5f19e1cd9
Marc Vanbrabant <marc@foreach.be>
2017-11-25 22:33:32 +0100

    Fix test on buildserver ?

--- test-projects/spring-boot-data-redis/src/main/java/ax/SpringBootDataRedis.java
+++ test-projects/spring-boot-data-redis/src/main/java/ax/SpringBootDataRedis.java
@@ -5,6 +5,7 @@ import com.foreach.across.modules.web.AcrossWebModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import redis.embedded.RedisServer;
+import redis.embedded.RedisServerBuilder;
 
 import java.io.IOException;
 
@@ -22,8 +23,12 @@ public class SpringBootDataRedis {
 
     @Bean(destroyMethod = "stop")
     public RedisServer redisServer() throws IOException {
-        RedisServer redisServer = new RedisServer(6379);
-        redisServer.start();
+	    RedisServer redisServer = new RedisServerBuilder().port( 6379 )
+	                                                      // https://github.com/kstyrc/embedded-redis/issues/51
+	                                                      // https://github.com/kstyrc/embedded-redis/issues/70
+	                                                      .setting( "maxmemory 128M" )
+	                                                      .build();
+	    redisServer.start();
         return redisServer;
     }
 }
