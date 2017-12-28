4bd16da6702a2f5d0729dfc57fc98c8aa46e220f
Steven Gentens <stg@foreach.be>
2017-12-28 08:24:11 +0100

    Spring Boot WebSocket fix test

--- test-projects/spring-boot-websocket/src/main/java/ax/application/controllers/GreetingController.java
+++ test-projects/spring-boot-websocket/src/main/java/ax/application/controllers/GreetingController.java
@@ -19,8 +19,8 @@ public class GreetingController
 
 	@MessageMapping("/hello")
 	@SendTo("/topic/greetings")
-	public GreetingMessage greeting( HelloMessage message) throws Exception {
-		Thread.sleep(1000); // simulated delay
-		return new GreetingMessage("Hello, " + message.getName() + "!"+ webSocketScopeBean.getUuid());
+	public GreetingMessage greeting( HelloMessage message ) throws Exception {
+		Thread.sleep( 1000 ); // simulated delay
+		return new GreetingMessage( "Hello, " + message.getName() + "! " + webSocketScopeBean.getUuid() );
 	}
 }
--- test-projects/spring-boot-websocket/src/main/java/ax/modules/websocket/config/WebSocketConfig.java
+++ test-projects/spring-boot-websocket/src/main/java/ax/modules/websocket/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package ax.modules.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
@@ -11,10 +10,6 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer
 {
-	@Override
-	public void configureMessageBroker( MessageBrokerRegistry config ) {
-	}
-
 	@Override
 	public void registerStompEndpoints( StompEndpointRegistry registry ) {
 		registry.addEndpoint( "/bar" ).withSockJS();
