cdc6199d60185ea9d9bd0cf28a981e1de9250ca8
Steven Gentens <stg@foreach.be>
2018-01-11 08:45:47 +0100

    Fix test elastic search

--- test-projects/spring-boot-data-elasticsearch/src/main/java/ax/application/business/Person.java
+++ test-projects/spring-boot-data-elasticsearch/src/main/java/ax/application/business/Person.java
@@ -2,15 +2,16 @@ package ax.application.business;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Document(indexName = "personidx", type = "person")
 public class Person
 {
-
 	@Id
 	private String id;
 
