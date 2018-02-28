48759e8a5c05d4bfab049e3492db06d2ebdd4a85
Marc Vanbrabant <marc@foreach.be>
2018-02-28 15:55:02 +0100

    Fix RedisRepositoriesAutoConfiguration

--- test-projects/spring-boot-data-redis/src/main/java/ax/SpringBootDataRedis.java
+++ test-projects/spring-boot-data-redis/src/main/java/ax/SpringBootDataRedis.java
@@ -3,11 +3,6 @@ package ax;
 import com.foreach.across.config.AcrossApplication;
 import com.foreach.across.modules.web.AcrossWebModule;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-import redis.embedded.RedisServer;
-import redis.embedded.RedisServerBuilder;
-
-import java.io.IOException;
 
 /**
  * @author Arne Vandamme
@@ -22,14 +17,4 @@ public class SpringBootDataRedis
 		SpringApplication.run( SpringBootDataRedis.class );
 	}
 
-	@Bean(destroyMethod = "stop")
-	public RedisServer redisServer() throws IOException {
-		RedisServer redisServer = new RedisServerBuilder().port( 6379 )
-		                                                  // https://github.com/kstyrc/embedded-redis/issues/51
-		                                                  // https://github.com/kstyrc/embedded-redis/issues/70
-		                                                  .setting( "maxmemory 128M" )
-		                                                  .build();
-		redisServer.start();
-		return redisServer;
-	}
 }
