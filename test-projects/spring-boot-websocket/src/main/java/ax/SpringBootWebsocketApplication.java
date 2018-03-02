c7cabcecc83ec0cdf7d6bc2aeb75bff51db66230
Arne Vandamme <arne@foreach.be>
2018-03-02 13:41:17 +0100

    Update websocket test

--- test-projects/spring-boot-websocket/src/main/java/ax/SpringBootWebsocketApplication.java
+++ test-projects/spring-boot-websocket/src/main/java/ax/SpringBootWebsocketApplication.java
@@ -1,8 +1,6 @@
 package ax;
 
 import ax.modules.websocket.CustomWebsocketModule;
-import com.foreach.across.autoconfigure.websocket.EnableWebSocket;
-import com.foreach.across.autoconfigure.websocket.EnableWebSocketMessageBroker;
 import com.foreach.across.config.AcrossApplication;
 import com.foreach.across.modules.web.AcrossWebModule;
 import org.springframework.boot.SpringApplication;
@@ -15,8 +13,6 @@ import org.springframework.context.annotation.Bean;
 @AcrossApplication(
 		modules = AcrossWebModule.NAME
 )
-@EnableWebSocketMessageBroker
-@EnableWebSocket
 public class SpringBootWebsocketApplication
 {
 	public static void main( String[] args ) {
