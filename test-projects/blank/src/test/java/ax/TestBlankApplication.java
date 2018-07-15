26bbc5865462c6dcc9baeff13dfa2ef95a37369e
Arne Vandamme <arne@foreach.be>
2018-07-15 13:18:45 +0200

    Fix Spring Data Cassandra support

--- test-projects/blank/src/test/java/ax/TestBlankApplication.java
+++ test-projects/blank/src/test/java/ax/TestBlankApplication.java
@@ -2,9 +2,6 @@ package ax;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
@@ -35,17 +32,18 @@ public class TestBlankApplication
 	@Test
 	public void shouldBootstrapAndReturnWhitelabelErrorPage() throws IOException {
 		Content content = Request.Get( "http://localhost:" + port ).addHeader( new BasicHeader( "Accept", "text/html" ) ).execute().handleResponse(
-				new ResponseHandler<Content>()
-				{
-					public Content handleResponse( HttpResponse response ) throws ClientProtocolException, IOException {
-						final HttpEntity entity = response.getEntity();
-						return entity == null ? null : new Content( EntityUtils.toByteArray( entity ), ContentType.getOrDefault( entity ) );
-					}
+				response -> {
+					final HttpEntity entity = response.getEntity();
+					return entity == null ? null : new Content( EntityUtils.toByteArray( entity ), ContentType.getOrDefault( entity ) );
 				} );
 		assertNotNull( content );
 		String body = content.asString();
-		assertTrue( "Body should contain whitelabel spring boot error page", StringUtils.contains( body,
-		                                                                                           "<html><body><h1>Whitelabel Error Page</h1><p>This application has no explicit mapping for /error, so you are seeing this as a fallback." ) );
+		assertTrue(
+				"Body should contain whitelabel spring boot error page",
+				StringUtils.contains(
+						body,
+						"<html><body><h1>Whitelabel Error Page</h1><p>This application has no explicit mapping for /error, so you are seeing this as a fallback." )
+		);
 
 	}
 }
