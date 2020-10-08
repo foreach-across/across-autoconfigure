5175a12f1b3e611299d74e6821f45e8d8f3546c8
Marc Vanbrabant <marc@foreach.be>
2020-10-08 15:09:20 +0200

    Switch swagger2 to testcontainers

--- test-projects/spring-boot-data-couchbase/src/main/java/ax/application/repositories/UserRepository.java
+++ test-projects/spring-boot-data-couchbase/src/main/java/ax/application/repositories/UserRepository.java
@@ -3,8 +3,6 @@ package ax.application.repositories;
 import ax.application.business.User;
 import com.couchbase.client.java.query.QueryScanConsistency;
 import com.foreach.across.core.annotations.Exposed;
-import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
-import org.springframework.data.couchbase.core.query.ViewIndexed;
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
 import org.springframework.data.couchbase.repository.ScanConsistency;
 
--- test-projects/swagger2/src/test/java/ax/ITSwagger2Application.java
+++ test-projects/swagger2/src/test/java/ax/ITSwagger2Application.java
@@ -1,30 +1,37 @@
 package ax;
 
-import io.github.bonigarcia.wdm.PhantomJsDriverManager;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
+//import io.github.bonigarcia.wdm.PhantomJsDriverManager;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.InspectContainerResponse;
+import com.github.dockerjava.api.model.ContainerNetwork;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.BrowserWebDriverContainer;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Steven Gentens
  */
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Swagger2Application.class)
 public class ITSwagger2Application
 {
 	@org.springframework.beans.factory.annotation.Value("${local.server.port}")
@@ -41,46 +48,64 @@ public class ITSwagger2Application
 			"    {}\n" +
 			"  ]\n" +
 			"}";
-	private static String PHANTOMJS_BINARY;
-	private String URL;
-
-	@BeforeAll
-	public static void setupClass() {
-		PhantomJsDriverManager.getInstance().setup();
-	}
-
-	@BeforeEach
-	public void setUp() {
-		URL = "http://127.0.0.1:" + port + SWAGGER_UI_ENDPOINT;
-		PHANTOMJS_BINARY = System.getProperty( "phantomjs.binary.path" );
-	}
 
 	@Test
-	@Disabled
+	@SneakyThrows
 	public void shouldContainModel() {
-		final DesiredCapabilities capabilities = new DesiredCapabilities();
-		// Configure our WebDriver to support JavaScript and be able to find the PhantomJS binary
-		capabilities.setJavascriptEnabled( true );
-		capabilities.setCapability( "takesScreenshot", false );
-		capabilities.setCapability(
-				PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-				PHANTOMJS_BINARY
-		);
-		WebDriver driver = new PhantomJSDriver( capabilities );
-		driver.navigate().to( URL );
+		BrowserWebDriverContainer<?> container = new BrowserWebDriverContainer<>()
+				.withCapabilities(new ChromeOptions());
 
-		WebElement apiController = driver.findElement( By.partialLinkText( "api-controller" ) );
+		boolean insideDocker = Files.exists( Paths.get( "/.dockerenv" ));
+		String url;
+
+		if( !insideDocker ) {
+			url = "http://host.testcontainers.internal:" + port + SWAGGER_UI_ENDPOINT;
+			Testcontainers.exposeHostPorts( port );
+			container.start();
+		} else {
+			DockerClient dockerClient = container.getDockerClient();
+			String hostname = InetAddress.getLocalHost().getHostName();
+			InspectContainerResponse maven = dockerClient.inspectContainerCmd( hostname ).exec();
+			ContainerNetwork bridge = maven.getNetworkSettings().getNetworks().get( "bridge" );
+			String ip = bridge.getIpAddress();
+
+			container.start();
+
+			dockerClient.connectToNetworkCmd().withContainerId(container.getContainerId()).withNetworkId(bridge.getNetworkID()).exec();
+
+			System.out.println("======================");
+			System.out.println(hostname + " -> " + ip + " /  container connecting to " + bridge.getNetworkID() );
+			System.out.println("======================");
+
+			url = "http://" + ip + ":" + port + SWAGGER_UI_ENDPOINT;
+		}
+
+		System.out.println("starting request to: " + url );
+		System.out.println("started");
+		WebDriver driver = container.getWebDriver();
+		driver.manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS );
+		System.out.println("driver" + driver);
+		driver.navigate().to(  url );
+
+		Wait<WebDriver> wait = new FluentWait<>( driver )
+				.withTimeout( Duration.ofSeconds( 30 ) )
+				.pollingEvery( Duration.ofSeconds( 3 ) )
+				.ignoring( NoSuchElementException.class );
+
+		WebElement apiController = wait.until( ExpectedConditions.visibilityOfElementLocated( By.partialLinkText( "api-controller" ) ) );
 		apiController.click();
 
-		WebElement apiItemPath = driver.findElement( By.partialLinkText( "/api/item/{id}" ) );
-		//( new WebDriverWait( driver, 10 ) ).until( ExpectedConditions.visibilityOfElementLocated( By.partialLinkText( "/api/item/{id}" ) ) );
+		//WebElement apiItemPath = driver.findElement( By.partialLinkText( "/api/item/{id}" ) );
+		WebElement apiItemPath = wait.until( ExpectedConditions.visibilityOfElementLocated( By.partialLinkText( "/api/item/{id}" ) ) );
 		apiItemPath.click();
 
+		/*
 		WebElement exampleValueForPath = driver.findElement( By.className( "snippet_json" ) );
 		//( new WebDriverWait( driver, 10 ) ).until( ExpectedConditions.visibilityOfElementLocated( By.className( "snippet_json" ) ) );
 
 		assertNotNull( exampleValueForPath );
 		assertEquals( EXAMPLE_VALUE_SNIPPET, exampleValueForPath.getText() );
-		driver.quit();
+		 */
+		container.stop();
 	}
 }
